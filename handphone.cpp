4cbe437b7b176130c2bf4bd2fc3b26669ca1073f
ImamRivaldi <32257746+ImamRivaldi@users.noreply.github.com>
2018-01-16 17:25:49 +0800

    awal
    meulah file handphone.cpp lwn deklarasi class

---
+++ handphone.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class handphone{
+	public:
+		void bemain (string mainan);
+		string design;
+		string camera;
+		int harga;
+};
+
+int main(){
+}
