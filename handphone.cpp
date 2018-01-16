23d5bf6e94bd2145643b30b2f589599564114ff0
ImamRivaldi <32257746+ImamRivaldi@users.noreply.github.com>
2018-01-16 17:33:29 +0800

    Ketiga
    Menampilkan isi yang di handaki dari variabel yg sdh d isi tadi

--- handphone.cpp
+++ handphone.cpp
@@ -16,4 +16,9 @@ int main(){
 	handphonesamsung.design="*Infinity Display \n*6.3'' screen \n*18.5:9 aspect ratio \n";
 	handphonesamsung.camera="*Dual Camera with Dual OIS \n*Optical Zoom \n*Dual Pixel Sensor \n*Bright Lens \n";
 	handphonesamsung.harga=12000000;
+	
+	cout<<"Samsung Galaxy Note 8: \n";
+	cout<<"Design"<<endl<<(handphonesamsung.design);
+	cout<<"Camera"<<endl<<(handphonesamsung.camera);
+	cout<<"Harga Rp."<<(handphonesamsung.harga)<<endl;
 }
